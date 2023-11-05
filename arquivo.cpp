f8ae6fbf7e4da5ef4196be41a3d860cbeee3f810
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2023-11-05 15:34:09 -0300

    Update file path for data source

--- arquivo.cpp
+++ arquivo.cpp
@@ -30,7 +30,7 @@ void Arquivo::abrir()
     if (!vetor)
         throw QString("Vetor nao existe");
     
-    QString endereco_arquivo = "/home/puc/repos/Atividade12EDII/dados/DadosDosAlunos_Matricula_Nome.csv";
+    QString endereco_arquivo = "/home/cpedrohenrique1/Documents/development/qtcreator/Atividade12EDII/dados/DadosDosAlunos_Matricula_Nome.csv";
     QFile arquivo(endereco_arquivo);
     if (!arquivo.open(QIODevice::ReadOnly))
         throw QString("Nao foi possivel abrir o arquivo");
